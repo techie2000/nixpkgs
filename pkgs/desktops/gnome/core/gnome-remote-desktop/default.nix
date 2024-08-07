b34d215917ef9446e1d67e2b748cf4b0727d6093
Bobby Rong <rjl931189261@126.com>
2024-08-07 13:35:24 +0000

    gnome.gnome-remote-desktop: 46.4 → 47.beta
    https://gitlab.gnome.org/GNOME/gnome-remote-desktop/-/compare/46.4...47.beta

--- pkgs/desktops/gnome/core/gnome-remote-desktop/default.nix
+++ pkgs/desktops/gnome/core/gnome-remote-desktop/default.nix
@@ -30,11 +30,11 @@
 
 stdenv.mkDerivation rec {
   pname = "gnome-remote-desktop";
-  version = "46.4";
+  version = "47.beta";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    hash = "sha256-KLwH6W2qFJy45NCldN8KLObsbOZHutyGFUzmEoIsxR4=";
+    hash = "sha256-ol7QJ0aCyF0x7eFOD/Tr5HDN7kDlmZNNFncAB/LFYsk=";
   };
 
   nativeBuildInputs = [
