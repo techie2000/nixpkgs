23db5f4ba7b64b3a7392353d3d1ce0c30ef92f36
Bobby Rong <rjl931189261@126.com>
2024-08-07 13:37:01 +0000

    libshumate: 1.2.3 → 1.3.beta
    https://gitlab.gnome.org/GNOME/libshumate/-/compare/1.2.3...1.3.beta

--- pkgs/development/libraries/libshumate/default.nix
+++ pkgs/development/libraries/libshumate/default.nix
@@ -22,14 +22,14 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "libshumate";
-  version = "1.2.3";
+  version = "1.3.beta";
 
   outputs = [ "out" "dev" "devdoc" ];
   outputBin = "devdoc"; # demo app
 
   src = fetchurl {
     url = "mirror://gnome/sources/libshumate/${lib.versions.majorMinor finalAttrs.version}/libshumate-${finalAttrs.version}.tar.xz";
-    hash = "sha256-TMbNc/bYcVX2J2arY+WqzEc72aDONZBpMqz8g56WTAw=";
+    hash = "sha256-VMOsrq0FRul8tMY6++U8ydK2irVCT/g/B7HZIWuuKys=";
   };
 
   depsBuildBuild = [
