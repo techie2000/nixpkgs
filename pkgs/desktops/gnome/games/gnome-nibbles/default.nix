042929a6020cb84aa61da1cae8e4a9b964e69169
Bobby Rong <rjl931189261@126.com>
2024-08-11 14:47:00 +0000

    gnome.gnome-nibbles: 4.0.4 → 4.1.rc1
    https://gitlab.gnome.org/GNOME/gnome-nibbles/-/compare/4.0.4...4.1.rc1

--- pkgs/desktops/gnome/games/gnome-nibbles/default.nix
+++ pkgs/desktops/gnome/games/gnome-nibbles/default.nix
@@ -20,11 +20,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "gnome-nibbles";
-  version = "4.0.4";
+  version = "4.1.rc1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-nibbles/${lib.versions.majorMinor finalAttrs.version}/gnome-nibbles-${finalAttrs.version}.tar.xz";
-    hash = "sha256-1xKkxpQ78ylWrfuSIvHxQ2mRHlTs67DNYffCWr16Wdo=";
+    hash = "sha256-JPSczTKz2IGVDGBT46EVLu4v/b9XfIbec+rJUzbQCqg=";
   };
 
   nativeBuildInputs = [
